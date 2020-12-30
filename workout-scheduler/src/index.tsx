b6bdec4b9c0e701f6b43d616be55074c450c7388
Sascha Majewsky <sascha.majewsky@pm.me>
2020-12-30 08:55:57 +0100

    Added client side rendering code from react native web guide

--- workout-scheduler/src/index.tsx
+++ workout-scheduler/src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { AppRegistry } from "react-native";
+
+// register the app
+AppRegistry.registerComponent("App", () => App);
+
+AppRegistry.runApplication("App", {
+  initialProps: {},
+  rootTag: document.getElementById("react-app"),
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
