a290c9a355de95f2bd325484094ad141176c1d2f
Sascha Majewsky <sascha.majewsky@pm.me>
2020-12-30 08:59:58 +0100

    Changed react entry/injection point to react native web

--- workout-scheduler/src/index.tsx
+++ workout-scheduler/src/index.tsx
@@ -1,26 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-
+/* import * as serviceWorker from "./serviceWorker"; */
 import { AppRegistry } from "react-native";
+import App from "./App";
 
 // register the app
 AppRegistry.registerComponent("App", () => App);
 
 AppRegistry.runApplication("App", {
   initialProps: {},
-  rootTag: document.getElementById("react-app"),
+  rootTag: document.getElementById("root"),
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.unregister();
+/* // If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service worker: http://bit.ly/CRA-PWA
+serviceWorker.unregister(); */
